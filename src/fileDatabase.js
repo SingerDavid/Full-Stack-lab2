927d0b1220c201208ff39f5b0d053660737373ad
David <dsinger1@hawk.iit.edu>
2023-02-14 17:44:12 -0600

    Connect to JSON database file

--- src/fileDatabase.js
+++ src/fileDatabase.js
@@ -1,7 +1,23 @@
 const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+
 const db = new Map();
-db.set('196a2223-0cfa-4a68-89f4-d79fd38d2cf5', {text: "this is temp 1", id: "196a2223-0cfa-4a68-89f4-d79fd38d2cf5"});
-db.set('2c93684d-4a73-4cf4-a8b7-4f6687482a99', {text: "this is temp 2", id: "2c93684d-4a73-4cf4-a8b7-4f6687482a99"});
+//db.set('196a2223-0cfa-4a68-89f4-d79fd38d2cf5', {text: "this is temp 1", id: "196a2223-0cfa-4a68-89f4-d79fd38d2cf5"});
+//db.set('2c93684d-4a73-4cf4-a8b7-4f6687482a99', {text: "this is temp 2", id: "2c93684d-4a73-4cf4-a8b7-4f6687482a99"});
+
+const loadData = () => {
+    const jsonData = fs.readFileSync(path.join(__dirname, '../data/data.json'));
+    const dataArray = JSON.parse(jsonData);
+    dataArray.forEach(element => {
+        db.set(element[0], element[1]);
+    });
+};
+
+const saveData = () => {
+    const stringifyData = JSON.stringify(Array.from(db));
+    fs.writeFileSync(path.join(__dirname, '../data/data.json'), stringifyData);
+};
 
 const repo = {
     findAll: () => Array.from(db.values()),
@@ -12,10 +28,20 @@ const repo = {
             text: temp.text
         };
         db.set(newData.id, newData)
+        saveData();
     },
-    deleteById: (uuid) => db.delete(uuid),
-    update: (temp) => db.set(temp.id, temp),
+    deleteById: (uuid) => {
+        db.delete(uuid);
+        saveData();
+    },
+    update: (temp) => {
+        db.set(temp.id, temp);
+        saveData();
+    },
 };
+
+//pull file from data on load
+loadData();
 
 //makes this a module to export for use elsewhere.
 module.exports = repo;
